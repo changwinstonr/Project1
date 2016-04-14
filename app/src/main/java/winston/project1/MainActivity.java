c85862a52f8d82ed26f225a07dbfd9e70a1f7b03
changwinstonr <chang.winstonr@gmail.com>
2016-04-14 14:40:26 -0700

    adds second activity/xml

--- app/src/main/java/winston/project1/MainActivity.java
+++ app/src/main/java/winston/project1/MainActivity.java
@@ -2,7 +2,6 @@ package winston.project1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -23,7 +22,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mStringList = new LinkedList<>();
-        mStringList.add("Input 1");
+        mStringList.add("Read");
+        mStringList.add("Learn");
+        mStringList.add("Exercise");
+        mStringList.add("Buy");
+
 
 
         mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mStringList);
@@ -47,75 +50,3 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
-/*import java.util.LinkedList;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-
-public class MainActivity extends extends AppCompatActivity {
-
-    final Context context = this;
-    private Button button;
-    private EditText result;
-
-    public void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-
-        // components from main.xml
-        button = (Button) findViewById(R.id.buttonPrompt);
-        result = (EditText) findViewById(R.id.editTextR);
-
-        // add button listener
-        button.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-
-                // get prompts.xml view
-                LayoutInflater li = LayoutInflater.from(context);
-                View promptsView = li.inflate(R.layout.prompt, null);
-
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-                        context);
-
-                // set prompts.xml to alertdialog builder
-                alertDialogBuilder.setView(promptsView);
-
-                final EditText userInput = (EditText) promptsView
-                        .findViewById(R.id.editText);
-
-                // set dialog message
-                alertDialogBuilder
-                        .setCancelable(false)
-                        .setPositiveButton("OK",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog,int id) {
-                                        // get user input and set it to result
-                                        // edit text
-                                        result.setText(userInput.getText());
-                                    }
-                                })
-                        .setNegativeButton("Cancel",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog,int id) {
-                                        dialog.cancel();
-                                    }
-                                });
-
-                // create alert dialog
-                AlertDialog alertDialog = alertDialogBuilder.create();
-
-                // show it
-                alertDialog.show();
-
-            }
-        });
-    }*/
