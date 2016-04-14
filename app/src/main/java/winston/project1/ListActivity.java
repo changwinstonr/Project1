39215a720f7e91c07f5695e65f034d8e78b4d636
changwinstonr <chang.winstonr@gmail.com>
2016-04-14 16:08:26 -0700

    removes simple_list_expandable_item_1, adds hide keyboard method

--- app/src/main/java/winston/project1/ListActivity.java
+++ app/src/main/java/winston/project1/ListActivity.java
@@ -1,13 +1,13 @@
-package winston.project1;
+/*package winston.project1;
 
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.AdapterView;
 
-/**
+*//**
  * Created by 4th3ist on 4/13/2016.
- */
+ *//*
 public class ListActivity extends Activity{
 
         AdapterView.OnItemClickListener;
@@ -19,4 +19,4 @@ public class ListActivity extends Activity{
         }
 
     }
-}
+}*/
--- app/src/main/java/winston/project1/MainActivity.java
+++ app/src/main/java/winston/project1/MainActivity.java
@@ -1,8 +1,11 @@
 package winston.project1;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -27,12 +30,20 @@ public class MainActivity extends AppCompatActivity {
         mStringList.add("Learn");
         mStringList.add("Buy");
 
-        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, mStringList);
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mStringList);
 
         ListView listName = (ListView) (findViewById(R.id.listView));
         listName.setAdapter(mAdapter);
         mEditText = (EditText) (findViewById(R.id.editText));
         mAddButton = (Button) (findViewById(R.id.addTextButton));
+/*        EditText edtView=(EditText)findViewById(R.id.editTextConvertValue);
+        edtView.setInputType(InputType.TYPE_NULL);*/
+
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
 
         mAddButton.setOnClickListener(new View.OnClickListener() {
             @Override
