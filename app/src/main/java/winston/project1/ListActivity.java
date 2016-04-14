c149b23efffb369ec944587d6db50b2904da6fe8
changwinstonr <chang.winstonr@gmail.com>
2016-04-14 15:31:13 -0700

    attempts to add simple_expandable_list_item_1

--- app/src/main/java/winston/project1/ListActivity.java
+++ app/src/main/java/winston/project1/ListActivity.java
@@ -1,7 +1,22 @@
 package winston.project1;
 
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.AdapterView;
+
 /**
  * Created by 4th3ist on 4/13/2016.
  */
-public class ListActivity {
+public class ListActivity extends Activity{
+
+        AdapterView.OnItemClickListener;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_list);
+        }
+
+    }
 }
--- app/src/main/java/winston/project1/MainActivity.java
+++ app/src/main/java/winston/project1/MainActivity.java
@@ -3,6 +3,7 @@ package winston.project1;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,12 +25,9 @@ public class MainActivity extends AppCompatActivity {
         mStringList = new LinkedList<>();
         mStringList.add("Read");
         mStringList.add("Learn");
-        mStringList.add("Exercise");
         mStringList.add("Buy");
 
-
-
-        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mStringList);
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, mStringList);
 
         ListView listName = (ListView) (findViewById(R.id.listView));
         listName.setAdapter(mAdapter);
