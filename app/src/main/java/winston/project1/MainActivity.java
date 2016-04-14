411fa026775b2cdb46527c949aba3772663cbcfc
changwinstonr <chang.winstonr@gmail.com>
2016-04-14 11:18:31 -0700

    adds EditText prompt

--- app/src/main/java/winston/project1/MainActivity.java
+++ app/src/main/java/winston/project1/MainActivity.java
@@ -9,6 +9,77 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.LinkedList;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends extends AppCompatActivity {
+
+    final Context context = this;
+    private Button button;
+    private EditText result;
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        // components from main.xml
+        button = (Button) findViewById(R.id.buttonPrompt);
+        result = (EditText) findViewById(R.id.editTextR);
+
+        // add button listener
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                // get prompts.xml view
+                LayoutInflater li = LayoutInflater.from(context);
+                View promptsView = li.inflate(R.layout.prompt, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+
+                // set prompts.xml to alertdialog builder
+                alertDialogBuilder.setView(promptsView);
+
+                final EditText userInput = (EditText) promptsView
+                        .findViewById(R.id.editText);
+
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        // get user input and set it to result
+                                        // edit text
+                                        result.setText(userInput.getText());
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+
+            }
+        });
+    }
 
 public class MainActivity extends AppCompatActivity {
     LinkedList<String> mStringList;
@@ -23,10 +94,7 @@ public class MainActivity extends AppCompatActivity {
 
         mStringList = new LinkedList<>();
         mStringList.add("Input 1");
-        mStringList.add("Input 2");
-        mStringList.add("Input 3");
-        mStringList.add("Input 4");
-        mStringList.add("Input 5");
+
 
         mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mStringList);
 
