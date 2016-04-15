8398c57c36847553f4f5f58fadde95e74dd78fe9
changwinstonr <chang.winstonr@gmail.com>
2016-04-15 13:49:27 -0700

    groups and comments out implement fail imports

--- app/src/main/java/winston/project1/MainActivity.java
+++ app/src/main/java/winston/project1/MainActivity.java
@@ -1,22 +1,22 @@
 package winston.project1;
 
-import android.content.Context;
+/*import android.content.Context;
+import android.text.InputType;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;*/ //unused: failed implements
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.InputType;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
--- app/src/main/java/winston/project1/ToDoActivity.java
+++ app/src/main/java/winston/project1/ToDoActivity.java
@@ -1,19 +1,21 @@
 package winston.project1;
 
-import android.content.Intent;
+/*import android.content.Intent;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;*/ //failed implements
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
-//import android.widget.Toast;
 
-//import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class ToDoActivity extends AppCompatActivity {
